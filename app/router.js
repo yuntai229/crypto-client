9c696fba270a0fbcb47351481c20c15ad8183ba5
yundy0605 <yundy@findata.com.tw>
2022-10-03 17:07:54 +0800

    coding style eslint

--- app/router.js
+++ app/router.js
@@ -4,10 +4,10 @@
  * @param {Egg.Application} app - egg application
  */
 module.exports = app => {
-	const { router, controller } = app;
-	router.get('/', controller.home.index);
+  const { router, controller } = app;
+  router.get('/', controller.home.index);
 
-	router.post('/marketcap', controller.token.createToken)
-	router.get('/marketcap/:tokenAddress', controller.token.getCurrentMarketCap)
-	router.get('/marketcap/:tokenAddress/history', controller.token.getHistoryMarketCap)
+  router.post('/marketcap', controller.token.createToken);
+  router.get('/marketcap/:tokenAddress', controller.token.getCurrentMarketCap);
+  router.get('/marketcap/:tokenAddress/history', controller.token.getHistoryMarketCap);
 };
--- app/service/api/reporter.js
+++ app/service/api/reporter.js
@@ -1,37 +1,37 @@
-'use strict'
+'use strict';
 
-const Service = require('egg').Service
+const Service = require('egg').Service;
 
 class ReportService extends Service {
-    async writeCurrentSpecifyTokenMarketCap(tokenAddress) {
-        try {
-            this.logger.info('[writeCurrentSpecifyTokenMarketCap] req: %s', JSON.stringify({ tokenAddress }))
-            const res = await this.ctx.curl(`http://${this.config.reporter.host}:${this.config.reporter.port}/writeCurrentSpecifyTokenMarketCap`, {
-                method: 'POST',
-                data: {
-                    tokenAddress
-                }
-            })
-        } catch (err) {
-            this.logger.error('[writeCurrentSpecifyTokenMarketCap] req: %s, res: %s', JSON.stringify({ tokenAddress }), err.message)
-            throw err
-        }
+  async writeCurrentSpecifyTokenMarketCap(tokenAddress) {
+    try {
+      this.logger.info('[writeCurrentSpecifyTokenMarketCap] req: %s', JSON.stringify({ tokenAddress }));
+      await this.ctx.curl(`http://${this.config.reporter.host}:${this.config.reporter.port}/writeCurrentSpecifyTokenMarketCap`, {
+        method: 'POST',
+        data: {
+          tokenAddress,
+        },
+      });
+    } catch (err) {
+      this.logger.error('[writeCurrentSpecifyTokenMarketCap] req: %s, res: %s', JSON.stringify({ tokenAddress }), err.message);
+      throw err;
     }
+  }
 
-    async writeSpecifyTokenMarketCapHistory(tokenAddress) {
-        try {
-            this.logger.info('[writeSpecifyTokenMarketCapHistory] req: %s', JSON.stringify({ tokenAddress }))
-            await this.ctx.curl(`http://${this.config.reporter.host}:${this.config.reporter.port}/writeSpecifyTokenMarketCapHistory`, {
-                method: 'POST',
-                data: {
-                    tokenAddress
-                }
-            })
-        } catch (err) {
-            this.logger.error('[writeSpecifyTokenMarketCapHistory] req: %s, res: %s', JSON.stringify({ tokenAddress }), err.message)
-            throw err
-        }
+  async writeSpecifyTokenMarketCapHistory(tokenAddress) {
+    try {
+      this.logger.info('[writeSpecifyTokenMarketCapHistory] req: %s', JSON.stringify({ tokenAddress }));
+      await this.ctx.curl(`http://${this.config.reporter.host}:${this.config.reporter.port}/writeSpecifyTokenMarketCapHistory`, {
+        method: 'POST',
+        data: {
+          tokenAddress,
+        },
+      });
+    } catch (err) {
+      this.logger.error('[writeSpecifyTokenMarketCapHistory] req: %s, res: %s', JSON.stringify({ tokenAddress }), err.message);
+      throw err;
     }
+  }
 }
 
-module.exports = ReportService
+module.exports = ReportService;
